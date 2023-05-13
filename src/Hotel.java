cec770db422ff9fadbce3e5466921d627c17b652
r0binn3082 <mrawan.shawkey2003@gmail.com>
2023-05-13 23:40:43 +0200

    ..

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,45 +1,43 @@
-
 import java.math.BigInteger;
 import java.util.*;
-/**
- *
- * @author lenovo
- */
 public class Hotel {
-     int x;
-    int y;
-      int count1st =0;
-    int[] floor1st= new int[20];
-     int[] floor2st= new int[20];
-    int count2st=0;
-        int[] floor3st= new int[20];
-    int count3st=0;
-     Scanner sc = new Scanner(System.in);
-    ArrayList<Integer> arrl=new ArrayList<Integer>();
-    public Hotel(){
-         for(int i=0;i<floor1st.length;i++){
-     floor1st[i]=i+101;
-    }
-         for(int i=0;i<floor2st.length;i++){
-      floor2st[i]=i+201;
-   } 
-         for(int i=0;i<floor3st.length;i++){
-         floor3st[i]=i+301;  
-     }
-    System.out.println("                    welcome to  our hotel ");
-        System.out.println("please enter your personal information\nenter yor name");
-        String n=sc.next();
-        System.out.println("enter your national id");
-        String id=sc.next();
-       System.out.println("1. for resevation press \n2. for check out\n3. see empty rooms");
-       x=sc.nextInt();
-        if(x==1){
-        System.out.println("what kind of room do you want to reserve\n1. single room\n2. double room\n3. full sweet");
-          y=sc.nextInt();
-        }
-          
-                  
+
+  protected int x;
+  protected int y;
+  protected int count1st =0;
+  protected int[] floor1st= new int[20];
+  protected int[] floor2st= new int[20];
+  protected int count2st=0;
+  protected int[] floor3st= new int[20];
+  protected int count3st=0;
+
+  Scanner sc = new Scanner(System.in);
+  ArrayList<Integer> arrl=new ArrayList<Integer>();
+
+  public Hotel(){
+  for(int i=0;i<floor1st.length;i++){
+    floor1st[i]=i+101;
+  }
+  for(int i=0;i<floor2st.length;i++){
+    floor2st[i]=i+201;
+  } 
+  for(int i=0;i<floor3st.length;i++){
+    floor3st[i]=i+301;  
+  }
+
+
+  System.out.println("                    welcome to  our hotel ");
+  System.out.println("please enter your personal information\nenter yor name");
+  String n=sc.next();
+
+  System.out.println("enter your national id");
+  String id=sc.next();
+
+  System.out.println("1. for resevation press \n2. for check out\n3. see empty rooms");
+  x=sc.nextInt();
+  if(x==1){
+    System.out.println("what kind of room do you want to reserve\n1. single room\n2. double room\n3. full sweet");
+    y=sc.nextInt();
+  }
+  }
 }
-      
-}
-    
--- src/rooms.java
+++ src/rooms.java
@@ -1,59 +1,63 @@
 import java.util.*;
 public class rooms extends Hotel {
-   
+
   public rooms(){
-       for(int i=0;i<floor1st.length;i++){
+    for(int i = 0 ; i < floor1st.length ; i++){
       arrl.add(floor1st[i]);
-      }
-      for(int i=0;i<floor2st.length;i++){
+    }
+    for(int i=0;i<floor2st.length;i++){
       arrl.add(floor2st[i]);
-      }
-      for(int i=0;i<floor3st.length;i++){
+    }
+    for(int i=0;i<floor3st.length;i++){
       arrl.add(floor3st[i]);
+    }
+  }
+
+  public void reservation(){
+    System.out.println("confirm your reservation \n1. yes\n2. no");
+    int z=sc.nextInt();
+    if(z==1){
+      if (count1st!=(floor1st.length-1)){
+        arrl.remove(floor1st[count1st]-101);
+        count1st++;
+        System.out.println("your reservation has been done");
       }
+    }
   }
-   public void reservation(){
-       System.out.println("confirm your reservation \n1. yes\n2. no");
-       int z=sc.nextInt();
-       if(z==1){
-       if (count1st!=(floor1st.length-1)){
-     arrl.remove(floor1st[count1st]-101);
-     count1st++;
-      System.out.println("your reservation has been done");
-       }
-        
-     }
+
+  public void reservation2(){
+    if (count2st!=(floor2st.length-1)){
+      arrl.remove(floor2st[count2st]-201);
+      count2st++;
+      System.out.println("your reservation has done 2");
+    }
   }
-   public void reservation2(){
-   
-     if (count2st!=(floor2st.length-1)){
-     arrl.remove(floor2st[count2st]-201);
-     count2st++;
-         System.out.println("your reservation has done 2");
-     }
-    }    
-    public void reservation(int x){
-   
-     if (count3st!=(floor3st.length-1)){
-     arrl.remove(floor3st[count3st]-301);
-       count3st++;
-}}
-     public void check_out(){
-         System.out.println("enter your room number");
-         int x=super.sc.nextInt();
-         if(x<150){
-         count1st--;
-         arrl.remove(floor1st[x-101]);
-         }else if(x<250){
-         count2st--;
-         arrl.remove(floor2st[x-201]);
-         }else if(x<321){
-         count3st--;
-         arrl.remove(floor3st[x-301]);
-         }else{System.out.println("the room number out of bound");}
-         
-     }
-         public void empty_rooms(){
-             System.out.println(arrl);
-         }
+
+  public void reservation(int x){
+    if (count3st!=(floor3st.length-1)){
+      arrl.remove(floor3st[count3st]-301);
+      count3st++;
+    }
+  }
+
+
+  public void check_out(){
+    System.out.println("enter your room number");
+    int x=super.sc.nextInt();
+    if(x<150){
+      count1st--;
+      arrl.remove(floor1st[x-101]);
+    }else if(x<250){
+      count2st--;
+      arrl.remove(floor2st[x-201]);
+    }else if(x<321){
+      count3st--;
+      arrl.remove(floor3st[x-301]);
+    }
+    else System.out.println("the room number out of bound"); 
+  }
+
+  public void empty_rooms(){
+    System.out.println(arrl);
+  }
 }
